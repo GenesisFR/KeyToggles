3cf80fdc714ce4932f76b522a2058d6529e42156
GenesisFR <david.castelmezzano@gmail.com>
2026-01-30 20:27:44 -0500

    + added support for AHKBatchScriptCompiler in the release script

--- release.bat
+++ release.bat
@@ -1,1 +1,5 @@
+if exist AHKBatchScriptCompiler.exe (
+	AHKBatchScriptCompiler.exe KeyToggles.ahk
+)
+
 powershell -Command Compress-Archive -Path "KeyToggles.exe", "KeyToggles.ini", "README.md" -Update -DestinationPath "KeyToggles.zip"
