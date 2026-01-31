ed45494725c819acbed561d5b91afe4efc0f1f13
GenesisFR <david.castelmezzano@gmail.com>
2026-01-31 17:15:49 -0500

    - shipping KeyToggles.ahk from now on for those who'd like to run the uncompiled script instead

--- release.bat
+++ release.bat
@@ -2,4 +2,4 @@ if exist AHKBatchScriptCompiler.exe (
 	AHKBatchScriptCompiler.exe KeyToggles.ahk
 )
 
-powershell -Command Compress-Archive -Path "KeyToggles.exe", "KeyToggles.ini", "README.md" -Update -DestinationPath "KeyToggles.zip"
+powershell -Command Compress-Archive -Path "KeyToggles.ahk", "KeyToggles.exe", "KeyToggles.ini", "README.md" -Update -DestinationPath "KeyToggles.zip"
