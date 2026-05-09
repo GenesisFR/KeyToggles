GenesisFR/KeyToggles

commit 3640f386a3af58799e2cadc8b6d74444cbecda32

+ added a viewer to display log messages - ticking the debug mode checkbox will now set the debug mode at runtime (a restart was needed before)

KeyToggles.ahk
@@ -233,6 +233,12 @@ GuiButtonSelect_Click(*)
 	g_guiWindowSelector.Show()
 }
 
+GuiButtonViewLog_Click(*)
+{
+	g_guiLog.Show()
+	ControlSend("^{End}", g_mapControls["editLog"])
+}
+
 GuiCB_Click(p_guiCtrl, *)
 {
 	switch p_guiCtrl
@@ -427,6 +433,7 @@ GuiCreate()
 
 	g_guiSettings.AddButton("Background" g_guiBackColor " x140 y" l_iTopY + l_iSpacingY * ++l_iCurrentRow + 15 " w100", "Reload").OnEvent("Click", GuiButtonReload_Click)
 	g_guiSettings.AddButton("Background" g_guiBackColor " x260 y" l_iTopY + l_iSpacingY * l_iCurrentRow + 15 " w100", "Save").OnEvent("Click", GuiButtonSave_Click)
+	g_guiSettings.AddButton("Background" g_guiBackColor " x380 y" l_iTopY + l_iSpacingY * l_iCurrentRow + 15 " w100", "View log").OnEvent("Click", GuiButtonViewLog_Click)
 
 	; Event handlers
 	for l_guiCtrl in g_guiSettings
@@ -534,6 +541,19 @@ GuiHK_Change(p_guiCtrl, *)
 	; Clear the corresponding DDL control if a hotkey is set
 	l_sDDLControlName := StrReplace(p_guiCtrl.Name, "hk", "ddl", , , 1)
 	g_mapControls[l_sDDLControlName].Choose(1)
+}
+
+GuiLogCreate()
+{
+	; Safety check
+	if IsSet(g_guiLog)
+		return
+
+	global g_guiLog := Gui.Call("Owner" g_guiSettings.Hwnd " -MinimizeBox -MaximizeBox", "Log Viewer")
+	g_guiLog.BackColor := g_guiBackColor
+	g_guiLog.SetFont("s10 " g_guiTextColor)
+	g_mapControls["editLog"] := g_guiLog.AddEdit((g_mapSettings["bDarkMode"] ? "BackgroundBlack" : "cBlack") " r25 w600 h400")
+	g_guiLog.AddButton("Background" g_guiBackColor " x515 y425 w100", "Clear").OnEvent("Click", (*) => g_mapControls["editLog"].Text := "")
 }
 
 GuiLV_DoubleClick(p_guiCtrl, p_iPosItem)
@@ -709,6 +729,7 @@ Init()
 	RestartAsAdminIfNeeded()
 	GuiApplyTheme()
 	GuiCreate()
+	GuiLogCreate()
 	SendMode(g_mapControls["ddlSendMode"].Text)
 	StartFocusCheck()
 	A_TrayMenu.Insert("&Suspend Hotkeys", "&Settings", (*) => GuiShow())
@@ -825,6 +846,20 @@ KeyToggle(p_sKey, p_bToggle, p_bWait := false)
 		KeyWait(p_sKey)
 
 	Output(A_ThisFunc "::end")
+}
+
+Log(p_sMessage, p_bSeparator := false)
+{
+	if (g_mapControls["cbDebugMode"].Value)
+	{
+		l_sFormattedTime := FormatTime(, "HH:mm:ss")
+		l_sMilliseconds := SubStr(A_TickCount, -3)
+		g_mapControls["editLog"].Text .= l_sFormattedTime "." l_sMilliseconds ": " p_sMessage "`r`n"
+		ControlSend("^{End}", g_mapControls["editLog"])
+
+		if (p_bSeparator)
+			g_mapControls["editLog"].Text .= "--------------------------------------------------`r`n"
+	}
 }
 
 ; Handle clicking outside the window while a mouse button is toggled
@@ -1041,12 +1076,14 @@ OnKeyPress(p_sThisHotkey)
 
 Output(p_sMessage, p_bSeparator := false)
 {
-	if (g_mapSettings["bDebugMode"])
+	Log(p_sMessage, p_bSeparator)
+
+	if (g_mapControls["cbDebugMode"].Value)
 	{
-		OutputDebug(p_sMessage "`n")
+		OutputDebug(p_sMessage "`r`n")
 
 		if (p_bSeparator)
-			OutputDebug("--------------------------------------------------`n")
+			OutputDebug("--------------------------------------------------`r`n")
 	}
 }
 
@@ -1487,7 +1524,7 @@ WriteConfigFile()
 }
 
 #SuspendExempt
-#HotIf g_mapSettings["bDebugMode"]
+#HotIf g_mapControls["cbDebugMode"].Value
 ; Exit script
 *!F10::ExitApp() ; ALT+F10
 

commit ba97205679e86244749d914eef13a21030f151e2

* fixed a regression from 3640f38 where assigning hotkeys was impossible due to Ctrl+End bleeding through

KeyToggles.ahk
@@ -72,6 +72,7 @@ g_bThemePopupShown := false
 g_bToggleKeysSnapshotTaken := false
 g_iWindowID := 0
 g_sConfigFileName := "KeyToggles.ini"
+g_sLogMessages := ""
 
 Init()
 
@@ -296,7 +297,7 @@ GuiButtonSelect_Click(*)
 
 GuiButtonViewLog_Click(*)
 {
-	g_guiLog.Show(g_mapSettings["bRememberWindowPositions"] ? "x" g_mapSettings["iLogWindowX"] " y" g_mapSettings["iLogWindowY"] : "")
+	g_guiLog.Show(g_mapSettings["bRememberWindowPositions"] ? "NoActivate x" g_mapSettings["iLogWindowX"] " y" g_mapSettings["iLogWindowY"] : "")
 	ControlSend("^{End}", g_mapControls["editLog"])
 }
 
@@ -317,9 +318,9 @@ GuiCB_Click(p_guiCtrl, *)
 	; We save the changes immediately to avoid having to hit Save
 	try
 	{
-		IniWrite(g_mapControls["cbAutorun"].Value, "KeyToggles.ini", "UI", "autorunMode")
-		IniWrite(g_mapControls["cbRestoreAutofiresOnFocus"].Value, "KeyToggles.ini", "UI", "restoreAutofiresOnFocus")
-		IniWrite(g_mapControls["cbRestoreTogglesOnFocus"].Value, "KeyToggles.ini", "UI", "restoreTogglesOnFocus")
+		IniWrite(g_mapSettings["bAutorun"], "KeyToggles.ini", "UI", "autorunMode")
+		IniWrite(g_mapSettings["bRestoreAutofiresOnFocus"], "KeyToggles.ini", "UI", "restoreAutofiresOnFocus")
+		IniWrite(g_mapSettings["bRestoreTogglesOnFocus"], "KeyToggles.ini", "UI", "restoreTogglesOnFocus")
 	}
 	catch as e
 		MsgBox(Format("{1}: {2}.`n`nFile:`t{3}`nLine:`t{4}`nWhat:`t{5}`nStack:`n{6}", type(e), e.Message, e.File, e.Line, e.What, e.Stack), , "Icon!")
@@ -960,11 +961,18 @@ Log(p_sMessage, p_bSeparator := false)
 	{
 		l_sFormattedTime := FormatTime(, "HH:mm:ss")
 		l_sMilliseconds := SubStr(A_TickCount, -3)
-		g_mapControls["editLog"].Text .= l_sFormattedTime "." l_sMilliseconds ": " p_sMessage "`r`n"
-		ControlSend("^{End}", g_mapControls["editLog"])
+		global g_sLogMessages .= l_sFormattedTime "." l_sMilliseconds ": " p_sMessage "`r`n"
 
 		if (p_bSeparator)
-			g_mapControls["editLog"].Text .= "--------------------------------------------------`r`n"
+			global g_sLogMessages .= "--------------------------------------------------`r`n"
+
+		if !WinActive("ahk_id " g_guiLog.Hwnd)
+		{
+			g_mapControls["editLog"].Text := g_sLogMessages
+
+			; Scroll to the end of the log
+			ScrollTo(g_guiLog.Hwnd, g_mapControls["editLog"], EditGetLineCount(g_mapControls["editLog"], "ahk_id " g_guiLog.Hwnd)) 
+		}
 	}
 }
 
@@ -1358,6 +1366,17 @@ RestartAsAdminIfNeeded()
 			ExitApp()
 		}
 	}
+}
+
+; https://www.autohotkey.com/board/topic/47704-scroll-to-line-in-edit-control/?p=297584
+ScrollTo(p_winID, p_editCtrl, p_iLineSel)
+{
+	l_iErrorLevel := SendMessage(0xCE, 0, 0, p_editCtrl, "ahk_id " p_winID) ; EM_GETFIRSTVISIBLELINE to get first line shown (results stored in ErrorLevel)
+	l_iLineSet := p_iLineSel - l_iErrorLevel -1	;get difference between current line and zeroth line, subtract one
+	PostMessage(0xB6, 0, l_iLineSet, p_editCtrl, "ahk_id " p_winID) ; EM_LINESCROLL to scroll target to top
+	p_iLineSel--	;EM_LINEINDEX is 0-based, so sub one
+	l_iErrorLevel := SendMessage(0xBB, p_iLineSel, 0, p_editCtrl, "ahk_id " p_winID) ; EM_LINEINDEX to get first char from line (results stored in ErrorLevel)
+	PostMessage(0xB1, l_iErrorLevel, l_iErrorLevel, p_editCtrl, "ahk_id " p_winID) ; EM_SETSEL to change caret to first character position of line 'lineSel'
 }
 
 SendAltTab(*)