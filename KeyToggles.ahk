GenesisFR/KeyToggles

commit 86e824da82436b027e08262c28245a1d0a0d79ed

- warning messages at launch are now shown after the GUI is displayed

KeyToggles.ahk
@@ -1499,8 +1499,8 @@ StartFocusCheck()
 	; Process name not valid or duplicate hotkeys, show the settings configurator
 	if (l_sMsgBoxText)
 	{
-		MsgBox(l_sMsgBoxText, , "Icon!")
 		GuiShow()
+		MsgBox(l_sMsgBoxText, , "Icon!")
 		return
 	}
 

commit 95615f688e1ee05665b7104491a40dfd925b02fb

* fixed a regression from 86e824d where the warning message box was displayed under the settings window if it was set to always on top

KeyToggles.ahk
@@ -1579,7 +1579,7 @@ StartFocusCheck()
 	if (l_sMsgBoxText)
 	{
 		GuiShow()
-		MsgBox(l_sMsgBoxText, , "Icon!")
+		MsgBox(l_sMsgBoxText, , "Icon! Owner" g_guiSettings.Hwnd)
 		return
 	}
 